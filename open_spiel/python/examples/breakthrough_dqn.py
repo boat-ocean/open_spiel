8cccfb42213c67cd2adb0a37d54214df552e6bb4
boat-ocean <72231906+boat-ocean@users.noreply.github.com>
2021-02-18 03:29:16 +0900

    increase column, row sizes

--- open_spiel/python/examples/breakthrough_dqn.py
+++ open_spiel/python/examples/breakthrough_dqn.py
@@ -82,7 +82,7 @@ def main(_):
   game = "breakthrough"
   num_players = 2
 
-  env_configs = {"columns": 5, "rows": 5}
+  env_configs = {"columns": 10, "rows": 10}
   env = rl_environment.Environment(game, **env_configs)
   info_state_size = env.observation_spec()["info_state"][0]
   num_actions = env.action_spec()["num_actions"]
